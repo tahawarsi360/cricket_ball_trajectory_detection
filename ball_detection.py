1ea4bd6c7dfb1b261f8e04efc9a4e339b9732b7c
Muhammad Taha Hussain <90678869+tahawarsi360@users.noreply.github.com>
2023-06-13 15:39:17 +0500

    Update ball_detection.py

--- ball_detection.py
+++ ball_detection.py
@@ -11,7 +11,7 @@ model.eval()
 COLORS = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (100, 255, 0), (100, 255, 100), (100, 100, 100)]
 
 # Path to the input videos folder
-videos_folder = 'trimmed'
+videos_folder = 'preprocessed_folder'
 # empty_frame = np.ones((height, width, 3), dtype=np.uint8) * 255
 COLORS = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (100, 255, 0), (100, 255, 100), (100, 100, 100)]
 COCO_INSTANCE_CATEGORY_NAMES = ['__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table', 'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
